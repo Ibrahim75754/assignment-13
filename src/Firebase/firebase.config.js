6032e9f20bcc71e05b4395f04af3095f3556b79a
Md. Ibrahim Hossain <ih75754@gmail.com>
2022-04-21 10:04:36 +0600

    first commit

---
+++ src/Firebase/firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBuia5nq5_1BzZ7P9t9OZw-RJLBcZ177ZY",
+  authDomain: "jewellery-4d2d0.firebaseapp.com",
+  projectId: "jewellery-4d2d0",
+  storageBucket: "jewellery-4d2d0.appspot.com",
+  messagingSenderId: "832740224911",
+  appId: "1:832740224911:web:2bf1441534c79a6ece65fa"
+};
+
+  export default firebaseConfig;
---
+++ src/components/Home/Banner.js
@@ -0,0 +1,55 @@
+import React from 'react';
+import banner1 from '../../img/banner1.jpg';
+import banner2 from '../../img/banner2.jpg';
+import banner3 from '../../img/banner3.jpg';
+import './banner.css';
+
+const Banner = () => {
+    return (
+        <div className=''>
+            <div id="carouselExampleDark" class="carousel carousel-dark slide" data-bs-ride="carousel">
+                <div class="carousel-indicators">
+                    <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+                    <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="1" aria-label="Slide 2"></button>
+                    <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="2" aria-label="Slide 3"></button>
+                </div>
+                <div class="carousel-inner ">
+                    <div class="carousel-item active" data-bs-interval="10000">
+                        <img src={banner3} class="d-block w-100  banner-height" alt="..." />
+                        <div class="carousel-caption d-none d-md-block caption-position">
+                            <h2 className="font-style">BEAUTY ____That Impress____</h2>
+                            <p>Enhance your beauty with our jewellery that is both spectacular and affordable.</p>
+                            <button className="btn btn-purchase">Explore</button>
+                        </div>
+                    </div>
+                    <div class="carousel-item" data-bs-interval="2000">
+                        <img src={banner2} class="d-block w-100  banner-height" alt="..." />
+                        <div class="carousel-caption d-none d-md-block caption-position text-light">
+                            <h2 className="">JEWELLERY ___That You'll Love___</h2>
+                            <p>We provide at Olimp,you will surely find even the rarest jewellery.</p>
+                            <button className=" btn btn-purchase">Explore</button>
+                        </div>
+                    </div>
+                    <div class="carousel-item">
+                        <img src={banner1} class="d-block w-100  banner-height" alt="..." />
+                        <div class="carousel-caption d-none d-md-block caption-position text-light">
+                            <h2 className="font-style">LOVE ___With New Collection___</h2>
+                            <p>Your Choice Is Our Best Collection.Purchase The Best jewellery Thats You Love.</p>
+                            <button className="btn btn-purchase">Explore</button>
+                        </div>
+                    </div>
+                </div>
+                <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleDark" data-bs-slide="prev">
+                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                    <span class="visually-hidden">Previous</span>
+                </button>
+                <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleDark" data-bs-slide="next">
+                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                    <span class="visually-hidden">Next</span>
+                </button>
+            </div>
+        </div>
+    );
+};
+
+export default Banner;
